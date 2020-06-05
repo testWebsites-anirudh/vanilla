037a97b533a077663c90a1a0d34a350d1bc05865
testWebsites-anirudh <65637619+testWebsites-anirudh@users.noreply.github.com>
2020-06-05 03:53:42 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 
+function navSlide(){
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav');
+    const overlay = document.querySelector('.overlay');
+
+    burger.addEventListener('click',()=>{
+
+        overlay.classList.toggle('overlay-active');
+        burger.classList.toggle('burger-active');
+        nav.classList.toggle('nav-active');
+
+        
+
+    });
+
+}
+
+
+navSlide();
